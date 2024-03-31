80a979c9db6571d9eeb7d7b310e7e4f73f310543
Jamen Kaye <jamen.kaye@gmail.com>
2024-03-31 00:42:44 -0700

    make Jack's changes to ReLU

--- yolov5/models/experimental.py
+++ yolov5/models/experimental.py
@@ -60,7 +60,7 @@ class MixConv2d(nn.Module):
             [nn.Conv2d(c1, int(c_), k, s, k // 2, groups=math.gcd(c1, int(c_)), bias=False) for k, c_ in zip(k, c_)]
         )
         self.bn = nn.BatchNorm2d(c2)
-        self.act = nn.SiLU()
+        self.act = nn.LeakyReLU(26/256, inplace=True) # was nn.SiLU()
 
     def forward(self, x):
         """Performs forward pass by applying SiLU activation on batch-normalized concatenated convolutional layer
